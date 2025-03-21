12099f88f3e9ba45e75ca1576611ad2cb715afe7
Jabamaca <julius.brian.moya.calma@gmail.com>
2025-03-22 05:35:27 +0800

    Update NuGet

--- JabaUtilsLibrary_UnitTest/UnitTest1.cs
+++ JabaUtilsLibrary_UnitTest/UnitTest1.cs
@@ -1,15 +1,4 @@
-using System;
-using Xunit;
-
 namespace JabaUtilsLibrary_UnitTest {
     public class UnitTest1 {
-
-        private class CloneTest : ICloneable {
-            public int a, b, c;
-
-            public object Clone () {
-                return this.MemberwiseClone ();
-            }
-        }
     }
 }
