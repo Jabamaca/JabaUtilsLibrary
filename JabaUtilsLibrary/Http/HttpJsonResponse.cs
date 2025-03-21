3c356c9b4b083bc9b24a3534d30e72bc7886d14b
Jabamaca <julius.brian.moya.calma@gmail.com>
2025-03-22 00:23:39 +0800

    Suppress Warniong

--- JabaUtilsLibrary/Http/HttpJsonResponse.cs
+++ JabaUtilsLibrary/Http/HttpJsonResponse.cs
@@ -3,13 +3,21 @@
 
         #region Properties
 
+#pragma warning disable IDE1006 // Naming Styles
         public string type {
+#pragma warning restore IDE1006 // Naming Styles
             get; set;
         }
+
+#pragma warning disable IDE1006 // Naming Styles
         public string message {
+#pragma warning restore IDE1006 // Naming Styles
             get; set;
         }
+
+#pragma warning disable IDE1006 // Naming Styles
         public T data {
+#pragma warning restore IDE1006 // Naming Styles
             get; set;
         }
 
